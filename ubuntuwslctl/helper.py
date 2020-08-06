7f7e44e840d8b7bb8c3d301fe90f291c73be8fcc
Jinming Wu, Patrick <me@patrickwu.space>
2020-08-06 13:57:26 +0800

    include query helper

--- ubuntuwslctl/helper.py
+++ ubuntuwslctl/helper.py
@@ -1,3 +1,4 @@
+import sys
 from configparser import ConfigParser
 
 
@@ -24,3 +25,37 @@ def get_ubuntu_wsl_conf_defaults():
         for key, val in config.items(section):
             the_conf_dict[section][key] = val
     return the_conf_dict
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    Adopted from https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")

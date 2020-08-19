8d8d1eba1e3f66feeb484be5778a994a866e7a6a
Jinming Wu, Patrick <me@patrickwu.space>
2020-08-19 15:04:38 +0800

    First test i18n implementation in main

--- ubuntuwslctl/main.py
+++ ubuntuwslctl/main.py
@@ -1,37 +1,40 @@
 import sys
+import gettext
 from argparse import ArgumentParser
 
 from .helper import config_name_extractor, query_yes_no
 from .loader import UbuntuWSLConfigEditor, WSLConfigEditor
 
+translation = gettext.translation('ubuntuwslctl', localedir=localedir, fallback=True)
+_ = translation.gettext
+
 
 class Application:
-
     def __init__(self):
         self.ubuntu_conf = UbuntuWSLConfigEditor()
         self.wsl_conf = WSLConfigEditor()
         self.parser = ArgumentParser(
-            description="%(prog)s is a tool for help manage your settings for Ubuntu WSL.")
+            description=_("ubuntuwsl is a tool for help manage your settings for Ubuntu WSL."))
         self._init_parser()
         self._args = self.parser.parse_args()
         self._args.func()
 
     def _init_parser(self):
         self.parser.add_argument(
-            '--version', action='version', version="%(prog)s 0.10")
+            '--version', action='version', version="ubuntuwsl 0.13")
         self.parser.set_defaults(func=self.do_help)
         self.parser.add_argument(
             "-y", "--yes", action="store_true",
-            help=("When passed, always assume yes."), required=False)
-        commands = self.parser.add_subparsers(title=("commands"))
+            help=_("When passed, always assume yes."), required=False)
+        commands = self.parser.add_subparsers(title=_("commands"))
 
         help_cmd = commands.add_parser(
             "help", aliases=["?"],
-            description=(
+            description=_(
                 "With no arguments, displays the list of ubuntuwslctl "
                 "commands. If a command name is given, displays the "
                 "description and options for the named command. "),
-            help=("Displays help about the specified command"))
+            help=_("Displays help about the specified command"))
         help_cmd.add_argument(
             "cmd", metavar="command", nargs='?',
             help=(
@@ -41,60 +44,60 @@ class Application:
 
         update_cmd = commands.add_parser(
             "update", aliases=["up"],
-            description=(
+            description=_(
                 "Change the value of one or more boot configuration "
                 "settings. To reset the value of a setting to its "
                 "default, simply omit the new value."),
-            help=("Change the state of one or more boot settings"))
+            help=_("Change the state of one or more boot settings"))
         update_cmd.add_argument(
             "name",
-            help=("The name of the configuration to be updated")
+            help=_("The name of the configuration to be updated")
         )
         update_cmd.add_argument(
             "value",
-            help=("The value you want to set for this configuration")
+            help=_("The value you want to set for this configuration")
         )
         update_cmd.set_defaults(func=self.do_update)
 
         reset_cmd = commands.add_parser(
             "reset", aliases=["rs", "rm"],
-            description=(
+            description=_(
                 "Reset(remove) the value of one configuration "
                 "settings."),
-            help=("Change the state of one or more boot settings"))
+            help=_("Change the state of one or more boot settings"))
         reset_cmd.add_argument(
             "name",
             nargs="?",
-            help=("The name of the configuration to be reset")
+            help=_("The name of the configuration to be reset")
         )
         reset_cmd.set_defaults(func=self.do_reset)
 
         show_cmd = commands.add_parser(
             "show", aliases=["cat"],
-            description=(
+            description=_(
                 "Display the specified stored configuration."),
-            help=("Show the specified stored configuration"))
+            help=_("Show the specified stored configuration"))
         show_cmd.add_argument(
             "name",
-            help=("The name of the boot configuration to display")
+            help=_("The name of the boot configuration to display")
         )
         show_cmd.add_argument(
             "-s", "--short", action="store_true",
-            help=("When enabled, only value will be displayed."))
+            help=_("When enabled, only value will be displayed."))
         show_cmd.add_argument(
             "-d", "--default", action="store_true",
-            help=("Show the default configuration settings instead of current "
-                  "user-defined ones."))
+            help=_("Show the default configuration settings instead of current "
+                   "user-defined ones."))
         show_cmd.set_defaults(func=self.do_show)
 
         ls_cmd = commands.add_parser(
             "list", aliases=["ls"],
-            description=("List all configurations."),
-            help=("List all configuration settings from ubuntu-wsl.conf and wsl.conf."))
+            description=_("List all configurations."),
+            help=_("List all configuration settings from ubuntu-wsl.conf and wsl.conf."))
         ls_cmd.add_argument(
             "-d", "--default", action="store_true",
-            help=("Show the default configuration settings instead of current "
-                  "user-defined ones."))
+            help=_("Show the default configuration settings instead of current "
+                   "user-defined ones."))
         ls_cmd.set_defaults(func=self.do_list)
 
     def _select_config(self, type_input):
@@ -103,7 +106,7 @@ class Application:
         elif type_input == "WSL":
             return self.wsl_conf
         else:
-            raise ValueError(("Invalid config name. Please check again."))
+            raise ValueError(_("Invalid config name. Please check again."))
 
     def do_help(self):
         if 'cmd' in self._args and self._args.cmd is not None:
@@ -118,22 +121,21 @@ class Application:
     def do_reset(self):
         assume_yes = 'yes' in self._args and self._args.yes
         if 'name' in self._args and self._args.name is not None:
-            if query_yes_no(("You are trying to reset all settings, "
-                             "including ubuntu-wsl.conf and wsl.conf. "
-                             "Do you still want to proceed?"), default="no", assume_yes=assume_yes):
+            if query_yes_no(_("You are trying to reset all settings, "
+                              "including ubuntu-wsl.conf and wsl.conf. "
+                              "Do you still want to proceed?"), default="no", assume_yes=assume_yes):
                 self._select_config("Ubuntu").resetall()
                 self._select_config("WSL").resetall()
         else:
             try:
                 config_type, config_section, config_setting = config_name_extractor(self._args.name)
-                if query_yes_no(("You are trying to reset "
-                                 + self._args.name + ". "
-                                 "Do you still want to proceed?"),
+                if query_yes_no(_("You are trying to reset `{name}`."
+                                  "Do you still want to proceed?").format(name=self._args.name),
                                 default="no", assume_yes=assume_yes):
                     self._select_config(config_type)\
                         .reset(config_section, config_setting)
             except KeyError:
-                print(("ERROR: Unknown keyname `{name}` passed.").format(name=self._args.name))
+                print(_("ERROR: Unknown key name `{name}` passed.").format(name=self._args.name))
                 sys.exit(1)
 
     def do_show(self):
@@ -142,7 +144,7 @@ class Application:
             self._select_config(config_type)\
                 .show(config_section, config_setting, self._args.short, self._args.default)
         except KeyError:
-            print(("ERROR: Unknown keyname `{name}` passed.").format(name=self._args.name))
+            print(_("ERROR: Unknown key name `{name}` passed.").format(name=self._args.name))
             sys.exit(1)
 
     def do_update(self):
@@ -151,7 +153,7 @@ class Application:
             self._select_config(config_type)\
                 .update(config_section, config_setting, self._args.value)
         except KeyError:
-            print(("ERROR: Unknown keyname `{name}` passed.").format(name=self._args.name))
+            print(_("ERROR: Unknown key name `{name}` passed.").format(name=self._args.name))
             sys.exit(1)
 
 

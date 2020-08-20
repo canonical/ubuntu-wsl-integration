706971600d69e2a0ed319208ec7bd7bcabcca75e
Jinming Wu, Patrick <me@patrickwu.space>
2020-08-20 21:47:25 +0800

    include default types for configurations

--- ubuntuwslctl/default.py
+++ ubuntuwslctl/default.py
@@ -4,6 +4,7 @@ from .helper import get_ubuntu_wsl_conf_defaults
 # these are the default settings in ubuntu-wsl.conf.
 # default configuration of ubuntu_wsl.conf is stored in /etc/default/wslu/
 default_ubuntu_wsl_conf_file = get_ubuntu_wsl_conf_defaults()
+default_ubuntu_wsl_conf_type = "bool"
 
 # these are the public default configuration for wsl.conf.
 # this won't cover all hidden configurations.
@@ -15,3 +16,12 @@ default_wsl_conf_file = {'automount': {'enabled': 'true',
                                      'generateResolvConf': 'true'},
                          'interop': {'enabled': 'true',
                                      'appendWindowsPath': 'true'}}
+
+default_wsl_conf_type = {'automount': {'enabled': 'bool',
+                                       'mountFsTab': 'bool',
+                                       'root': 'path',
+                                       'options': 'mount'},
+                         'network': {'generateHosts': 'bool',
+                                     'generateResolvConf': 'bool'},
+                         'interop': {'enabled': 'bool',
+                                     'appendWindowsPath': 'bool'}}

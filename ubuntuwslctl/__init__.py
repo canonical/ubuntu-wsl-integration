8aa7ca4ab249f45b77e5cc9ededa32e599783d8f
Jinming Wu, Patrick <me@patrickwu.space>
2020-08-21 16:06:55 +0800

    Include license header

---
+++ ubuntuwslctl/__init__.py
@@ -0,0 +1,19 @@
+#     ubuntuwslctl
+#     Copyright (C) 2020 Canonical Ltd.
+#                   2020 Patrick Wu <patrick.wu@canonical.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This package is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+#  On Debian systems, the complete text of the GNU General
+#  Public License version 3 can be found in "/usr/share/common-licenses/GPL-3".
--- ubuntuwslctl/default.py
+++ ubuntuwslctl/default.py
@@ -1,4 +1,25 @@
-# This is the file that contains the default settings if reset
+#    ubuntuwslctl.default - storing default values
+#    Copyright (C) 2020 Canonical Ltd.
+#    Copyright (C) 2020 Patrick Wu
+#
+#    Authors: Patrick Wu <patrick.wu@canonical.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This package is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+#  On Debian systems, the complete text of the GNU General
+#  Public License version 3 can be found in "/usr/share/common-licenses/GPL-3".
+
 from .helper import get_ubuntu_wsl_conf_defaults
 
 # these are the default settings in ubuntu-wsl.conf.

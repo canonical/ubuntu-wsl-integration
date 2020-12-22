01db8af8372a390db4a89c816cfda945d57a6e36
Jinming Wu, Patrick <me@patrickwu.space>
2020-12-22 16:22:19 +0800

    Code cleanup

--- ubuntuwslctl/core/default.py
+++ ubuntuwslctl/core/default.py
@@ -20,34 +20,6 @@
 #  On Debian systems, the complete text of the GNU General
 #  Public License version 3 can be found in "/usr/share/common-licenses/GPL-3".
 
-# old settings system
-# from ubuntuwslctl.utils.helper import get_ubuntu_wsl_conf_defaults
-#
-# # these are the default settings in ubuntu-wsl.conf.
-# # default configuration of ubuntu_wsl.conf is stored in /etc/default/wslu/
-# default_ubuntu_wsl_conf_file = get_ubuntu_wsl_conf_defaults()
-# default_ubuntu_wsl_conf_type = "bool"
-#
-# # these are the public default configuration for wsl.conf.
-# # this won't cover all hidden configurations.
-# default_wsl_conf_file = {'automount': {'enabled': 'true',
-#                                        'mountFsTab': 'true',
-#                                        'root': '/mnt/',
-#                                        'options': ''},
-#                          'network': {'generateHosts': 'true',
-#                                      'generateResolvConf': 'true'},
-#                          'interop': {'enabled': 'true',
-#                                      'appendWindowsPath': 'true'}}
-# # type is flatten as case is not important for config items
-# default_wsl_conf_type = {'automount': {'enabled': 'bool',
-#                                        'mountfstab': 'bool',
-#                                        'root': 'path',
-#                                        'options': 'mount'},
-#                          'network': {'generatehosts': 'bool',
-#                                      'generateresolvconf': 'bool'},
-#                          'interop': {'enabled': 'bool',
-#                                      'appendwindowspath': 'bool'}}
-
 ## Tooltip and Name definition ##
 
 conf_def = {
--- ubuntuwslctl/tui.py
+++ ubuntuwslctl/tui.py
@@ -1,28 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
+#    ubuntuwslctl.tui - TUI
+#    Copyright (C) 2020 Canonical Ltd.
+#    Copyright (C) 2020 Patrick Wu
 #
-# Urwid tour.  It slices, it dices..
-#    Copyright (C) 2004-2011  Ian Ward
+#    Authors: Patrick Wu <patrick.wu@canonical.com>
 #
-#    This library is free software; you can redistribute it and/or
-#    modify it under the terms of the GNU Lesser General Public
-#    License as published by the Free Software Foundation; either
-#    version 2.1 of the License, or (at your option) any later version.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-#    This library is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    Lesser General Public License for more details.
+#  This package is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Lesser General Public
-#    License along with this library; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
-# Urwid web site: http://excess.org/urwid/
-
-"""
-Urwid tour.  Shows many of the standard widget types and features.
-"""
+#  On Debian systems, the complete text of the GNU General
+#  Public License version 3 can be found in "/usr/share/common-licenses/GPL-3".
 
 import urwid
 import urwid.raw_display

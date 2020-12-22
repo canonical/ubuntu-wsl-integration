d42b6b9e38496a5111e567be3fbadf12f96e4395
Jinming Wu, Patrick <me@patrickwu.space>
2020-12-22 15:30:44 +0800

    Test tooltip

--- ubuntuwslctl/tui.py
+++ ubuntuwslctl/tui.py
@@ -30,17 +30,22 @@ from ubuntuwslctl.core.generator import SuperHandler
 from ubuntuwslctl.core.default import conf_def
 from ubuntuwslctl.utils.helper import str2bool
 
+def tui_edit(content, default, tooltip):
+    text = content+u": "
+    set = urwid.Pile([
+        urwid.AttrWrap(urwid.Edit(('editcp', text), default), 'editbx', 'editfc'),
+        urwid.Padding(urwid(urwid.Text(tooltip), ('relative', len(text))))
+    ])
+    return urwid.Padding(set, left=2, right=2)
 
 def tui_main(ubuntu, wsl):
     text_header = u"Ubuntu WSL Configuration UI (Experimental)"
     text_footer = u"UP / DOWN / PAGE UP / PAGE DOWN: scroll F5: save F8: exit"
-    config = SuperHandler(ubuntu, wsl, '', 'json').get_config()
+    config = SuperHandler(ubuntu, wsl, '', 'json').get_config(False)
 
     # general_text = lambda x : urwid.Padding(urwid.Text(x), left=2, right=2)
     general_title = lambda x: urwid.Padding(urwid.Text(('important', x)), left=2, right=2, min_width=20)
     general_subtitle = lambda x: urwid.Padding(urwid.Text(('subimportant', x)), left=2, right=2, min_width=20)
-    general_edit = lambda x, y: urwid.Padding(urwid.AttrWrap(urwid.Edit(('editcp', x), y), 'editbx', 'editfc'), left=2,
-                                              width=50)
     general_checkbox = lambda x, y: urwid.Padding(urwid.CheckBox(x, state=y), left=2, right=2)
 
     blank = urwid.Divider()
@@ -63,7 +68,7 @@ def tui_main(ubuntu, wsl):
                     if j_tmp[k].lower() in ("yes", "no", "1", "0", "true", "false"):
                         listbox_content.append(general_checkbox(conf_def[i][j][k]['_friendly_name'], str2bool(j_tmp[k])))
                     else:
-                        listbox_content.append(general_edit(conf_def[i][j][k]['_friendly_name'], j_tmp[k]))
+                        listbox_content.append(tui_edit(conf_def[i][j][k]['_friendly_name'], j_tmp[k], conf_def[i][j][k]['tip']))
             listbox_content.append(blank)
 
     header = urwid.AttrWrap(urwid.Text(text_header), 'header')

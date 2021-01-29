d2ba79ed55a30bd31e5fbd8363511a0a80faea13
Jinming Wu, Patrick <me@patrickwu.space>
2021-01-29 11:07:57 +0800

    Upgrade to 0.27.0

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='ubuntuwslctl',
-      version='0.26.0',
+      version='0.27.0',
       description="Ubuntu WSL Utility to manage Ubuntu WSL settings",
       long_description=long_description,
       long_description_content_type="text/markdown",

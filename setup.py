67421804cd62d4f6f34f887fa2da94ca1b2293b7
Jinming Wu, Patrick <me@patrickwu.space>
2021-02-04 20:07:03 +0800

    bump version to 0.28

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='ubuntuwslctl',
-      version='0.27.2',
+      version='0.28.0',
       description="Ubuntu WSL Utility to manage Ubuntu WSL settings",
       long_description=long_description,
       long_description_content_type="text/markdown",

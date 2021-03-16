0989ff89201612e623fb24a163d0a30d1f9bafc3
Jinming Wu, Patrick <me@patrickwu.space>
2021-03-16 15:51:35 +0800

    Update to 0.29.0

--- ubuntuwslctl/main.py
+++ ubuntuwslctl/main.py
@@ -65,7 +65,7 @@ class Application:
 
     def _init_parser(self):
         self.parser.add_argument(
-            '--version', action='version', version="ubuntuwsl 0.28.0")
+            '--version', action='version', version="ubuntuwsl 0.29.0")
         self.parser.set_defaults(func=self.do_help)
         self.parser.add_argument(
             "-y", "--yes", action="store_true",
